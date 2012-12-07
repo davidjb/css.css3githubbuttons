ea3748e3d18002d9a57c76597afc0c77a92b62d4
David\ Beitey <david@davidjb.com>
2012-12-07 11:12:58 +1000

    Switch to using CodeFusion's library with colours, extra icons, fixes, sizes and more

--- css/css3githubbuttons/__init__.py
+++ css/css3githubbuttons/__init__.py
@@ -1,4 +1,4 @@
-from fanstatic import Library, Resource
+from fanstatic import Library, Resource, Group
 
 library = Library('css3_github_buttons',
                   'resources/css3-github-buttons',
@@ -11,6 +11,16 @@ library = Library('css3_github_buttons',
 buttons = Resource(library,
                    'gh-buttons.css'
                   )
+buttons_ext_sizes = Resource(library,
+                             'ext_button_size/gh-buttons_size.css',
+                             depends=(buttons,)
+                            )
+buttons_ext_icons = Resource(library,
+                             'ext_button_icons/gh-buttons_ext_icons.css',
+                             depends=(buttons,)
+                            )
+buttons_ext_all = Group([buttons_ext_sizes, buttons_ext_icons])
+
 
 # Define the resources in the library like this.
 # For options and examples, see the fanstatic documentation.
